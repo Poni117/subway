96a44bc65bfaef401231d2a64f7880b403907ab2
rZinnatov <roman.zinnatov@gmail.com>
2017-11-01 00:51:56 +0400

    Initial commit

---
+++ Metro/Branch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro
+{
+    class Branch
+    {
+        private string _name;
+        private string _color;
+        private int _number;
+        private string[] _stations;
+
+        public Branch(string name, string color, int number, string[] stations)
+        {
+            _name = name;
+            _color = color;
+            _number = number;
+            _stations = stations;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public string GetColor()
+        {
+            return _color;
+        }
+
+        public int GetNumber()
+        {
+            return _number;
+        }
+
+        public string[] GetStations()
+        {
+            return _stations;
+        }
+    }
+
+    
+}
---
+++ Metro/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Branch branch = new Branch("Кировско-Выборгская", "красная", 1, new string[]
+                {
+                    "Девяткино",
+                    "Гражданский проспект",
+                    "Академическая",
+                    "Политехническая",
+                    "Площадь Мужества",
+                    "Лесная",
+                    "Выборгская",
+                    "Площадь Ленина",
+                    "Чернышевская",
+                    "Площадь Восстания",
+                    "Владимирская",
+                    "Пушкинская",
+                    "Технологический институт",
+                    "Балтийская",
+                    "Нарвская",
+                    "Кировский завод",
+                    "Автово",
+                    "Ленинский проспект",
+                    "Проспект Ветеранов"
+                }
+            );
+            Console.WriteLine(branch.GetNumber() + branch.GetColor() + branch.GetName());
+            for (int i = 0; i < branch.GetStations().Length; i++)
+            {
+                Console.WriteLine(branch.GetStations()[i]);
+            }
+        }
+    }
+}
